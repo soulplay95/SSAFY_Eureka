18f94a0bbeac30f0175c90531ae57ffcf150c605
jejin009 <dmsine009@naver.com>
2021-06-12 14:16:15 +0900

    member/qna controller 생성

---
+++ Back/eureka/src/main/java/com/ssafy/eureka/dto/Comment.java
@@ -0,0 +1,20 @@
+package com.ssafy.eureka.dto;
+
+public class Comment {
+
+	int temp1;
+	int temp2;
+	public int getTemp1() {
+		return temp1;
+	}
+	public void setTemp1(int temp1) {
+		this.temp1 = temp1;
+	}
+	public int getTemp2() {
+		return temp2;
+	}
+	public void setTemp2(int temp2) {
+		this.temp2 = temp2;
+	}
+	
+}
---
+++ Back/eureka/src/main/java/com/ssafy/eureka/dto/QnA.java
@@ -0,0 +1,5 @@
+package com.ssafy.eureka.dto;
+
+public class QnA {
+
+}
