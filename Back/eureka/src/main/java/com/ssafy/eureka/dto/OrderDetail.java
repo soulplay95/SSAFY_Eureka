9e8f945b60175e6a018368a6113f30f8bb8067d7
jejin009 <dmsine009@naver.com>
2021-06-12 15:07:15 +0900

    dto 생성

---
+++ Back/eureka/src/main/java/com/ssafy/eureka/dto/OrderDetail.java
@@ -0,0 +1,43 @@
+package com.ssafy.eureka.dto;
+
+public class OrderDetail {
+
+	int orderdetail_id;
+	String orderdetail_count;
+	String orderdetail_price;
+	int product_id;
+	int order_id;
+	
+	public int getOrderdetail_id() {
+		return orderdetail_id;
+	}
+	public void setOrderdetail_id(int orderdetail_id) {
+		this.orderdetail_id = orderdetail_id;
+	}
+	public String getOrderdetail_count() {
+		return orderdetail_count;
+	}
+	public void setOrderdetail_count(String orderdetail_count) {
+		this.orderdetail_count = orderdetail_count;
+	}
+	public String getOrderdetail_price() {
+		return orderdetail_price;
+	}
+	public void setOrderdetail_price(String orderdetail_price) {
+		this.orderdetail_price = orderdetail_price;
+	}
+	public int getProduct_id() {
+		return product_id;
+	}
+	public void setProduct_id(int product_id) {
+		this.product_id = product_id;
+	}
+	public int getOrder_id() {
+		return order_id;
+	}
+	public void setOrder_id(int order_id) {
+		this.order_id = order_id;
+	}
+	
+	
+}
---
+++ Back/eureka/src/main/java/com/ssafy/eureka/dto/Product.java
@@ -0,0 +1,83 @@
+package com.ssafy.eureka.dto;
+
+public class Product {
+	int product_id;
+	String product_name;
+	String product_category;
+	String product_price;
+	String product_img;
+	String product_rating;
+	String product_brand;
+	String product_delivery;
+	String product_detailimg;
+	String product_count;
+	String member_userid;
+	public int getProduct_id() {
+		return product_id;
+	}
+	public void setProduct_id(int product_id) {
+		this.product_id = product_id;
+	}
+	public String getProduct_name() {
+		return product_name;
+	}
+	public void setProduct_name(String product_name) {
+		this.product_name = product_name;
+	}
+	public String getProduct_category() {
+		return product_category;
+	}
+	public void setProduct_category(String product_category) {
+		this.product_category = product_category;
+	}
+	public String getProduct_price() {
+		return product_price;
+	}
+	public void setProduct_price(String product_price) {
+		this.product_price = product_price;
+	}
+	public String getProduct_img() {
+		return product_img;
+	}
+	public void setProduct_img(String product_img) {
+		this.product_img = product_img;
+	}
+	public String getProduct_rating() {
+		return product_rating;
+	}
+	public void setProduct_rating(String product_rating) {
+		this.product_rating = product_rating;
+	}
+	public String getProduct_brand() {
+		return product_brand;
+	}
+	public void setProduct_brand(String product_brand) {
+		this.product_brand = product_brand;
+	}
+	public String getProduct_delivery() {
+		return product_delivery;
+	}
+	public void setProduct_delivery(String product_delivery) {
+		this.product_delivery = product_delivery;
+	}
+	public String getProduct_detailimg() {
+		return product_detailimg;
+	}
+	public void setProduct_detailimg(String product_detailimg) {
+		this.product_detailimg = product_detailimg;
+	}
+	public String getProduct_count() {
+		return product_count;
+	}
+	public void setProduct_count(String product_count) {
+		this.product_count = product_count;
+	}
+	public String getMember_userid() {
+		return member_userid;
+	}
+	public void setMember_userid(String member_userid) {
+		this.member_userid = member_userid;
+	}
+	
+	
+}
