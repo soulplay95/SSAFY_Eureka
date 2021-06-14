f9234a022dac7f85a21482723037d254d9b7e5be
sangmin song <ssmin940606@gmail.com>
2021-06-14 21:37:56 +0900

    create:new branch for Front

--- Front/eureka-front/src/store/index.js
+++ Front/eureka-front/src/store/index.js
@@ -1,7 +1,14 @@
 import { createStore } from "vuex";
 
 export default createStore({
-  state: {},
+  state: {
+    // 유저 정보
+    // 장바구니 (로그인할 때 리셋)
+    // 검색 정보
+    // 메인 페이지 상품 정보
+    // Footer 정보
+
+  },
   mutations: {},
   actions: {},
   modules: {},
