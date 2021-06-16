2e970cd98645ae447c14c8cac2693857e37fb745
sqk8657 <sqk86577@gmail.com>
2021-06-16 21:35:30 +0900

    feat: product기능 추가시작

--- Back/eureka/src/main/java/com/ssafy/eureka/dao/ProductDao.java
+++ Back/eureka/src/main/java/com/ssafy/eureka/dao/ProductDao.java
@@ -6,6 +6,8 @@ import java.util.Map;
 import org.apache.ibatis.annotations.Mapper;
 
 import com.ssafy.eureka.dto.Product;
+import com.ssafy.eureka.dto.Productqna;
+import com.ssafy.eureka.dto.Review;
 
 @Mapper
 public interface ProductDao {
@@ -14,12 +16,16 @@ public interface ProductDao {
 
 	List<Product> showItemList(String category);
 
-	Map<String, Object> showDetail(String product_id);
+	Product getProduct(String product_id);
+	Productqna getProductqna(String product_id);
+	Review getReview(String product_id);
 
 	int registProduct(Product product);
 
 	int modifyProduct(Product product);
 
 	int deleteProduct(String product_id);
+
+
 	
 }
