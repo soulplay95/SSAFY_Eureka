9ae2e20a7b31d7ee95c062839c2932f9c9d0dd5d
soulplay95 <soulplay95@gmail.com>
2021-06-21 21:29:33 +0900

    feat: 장바구니 기능 추가

--- Front/eureka-front/src/components/Cart/TotalOrder.vue
+++ Front/eureka-front/src/components/Cart/TotalOrder.vue
@@ -1,4 +1,5 @@
 <template>
+  <!-- 주문표 -->
   <div
     class="
       bo9
@@ -13,11 +14,15 @@
     <!--  -->
     <div class="flex-w flex-sb bo10 p-t-15 p-b-20">
       <span class="s-text18 w-size19 w-full-sm"> 총 상품수 : </span>
-      <span class="m-text21 w-size20 w-full-sm"> 00개 </span>
+      <span class="m-text21 w-size20 w-full-sm">
+        {{ checked.length }}
+      </span>
     </div>
     <div class="flex-w flex-sb bo10 p-t-15 p-b-20">
       <span class="s-text18 w-size19 w-full-sm"> 총 상품금액 : </span>
-      <span class="m-text21 w-size20 w-full-sm"> 00원 </span>
+      <span class="m-text21 w-size20 w-full-sm">
+        {{ $filters.price(totalProductPrice) }}원
+      </span>
     </div>
     <div class="flex-w flex-sb bo10 p-t-15 p-b-20">
       <span class="s-text18 w-size19 w-full-sm"> 총 할인금액: </span>
@@ -25,14 +30,18 @@
     </div>
     <div class="flex-w flex-sb bo10 p-t-15 p-b-20">
       <span class="s-text18 w-size19 w-full-sm"> 총 배송비: </span>
-      <span class="m-text21 w-size20 w-full-sm"> 00원 </span>
+      <span class="m-text21 w-size20 w-full-sm">
+        {{ $filters.price(totalDeliveryPrice) }}원
+      </span>
     </div>
 
     <!--  -->
     <div class="flex-w flex-sb-m p-t-26 p-b-30">
       <span class="m-text22 w-size19 w-full-sm"> Total: </span>
 
-      <span class="m-text21 w-size20 w-full-sm"> 39,000원 </span>
+      <span class="m-text21 w-size20 w-full-sm">
+        {{ $filters.price(totalPrice) }}원
+      </span>
     </div>
 
     <div class="size15 trans-0-4">
@@ -45,7 +54,37 @@
 </template>
 
 <script>
-export default {};
+export default {
+  computed: {
+    // 장바구니 리스트 가져오기
+    items: {
+      get() {
+        return this.$store.state.cart.items;
+      },
+      set(value) {
+        this.$store.state.cart.items = value;
+      },
+    },
+    checked: {
+      get() {
+        return this.$store.state.cart.checked;
+      },
+      set(value) {
+        this.$store.state.cart.checked = value;
+      },
+    },
+    totalProductPrice() {
+      return this.$store.getters['cart/totalProductPrice'];
+    },
+    totalDeliveryPrice() {
+      return this.$store.getters['cart/totalDeliveryPrice'];
+    },
+    totalPrice() {
+      return this.$store.getters['cart/totalPrice'];
+    },
+  },
+  methods: {},
+};
 </script>
 
 <style></style>
--- Front/eureka-front/src/store/modules/Cart/cart.js
+++ Front/eureka-front/src/store/modules/Cart/cart.js
@@ -2,13 +2,16 @@
 export const cart = {
   namespaced: true,
   state: () => ({
+    // 장바구니 리스트
+    // 회원의 경우 userid로 서버와 통신하여 얻어온다.
+    // 비회원의 경우
+    // 상품 id, 수량
     items: [
       {
         id: 1,
         image: 'https://picsum.photos/720/960/?image=476',
-        title: 'Herschel supply co 25l',
-        link: '',
-        price: 75000,
+        title: '비비고 열무김치',
+        price: 750020,
         badge: 'new',
         quantity: 1,
         deliveryCharge: 2500,
@@ -17,8 +20,7 @@ export const cart = {
       {
         id: 2,
         image: 'https://picsum.photos/720/960/?image=478',
-        title: 'Denim jacket blue',
-        link: '',
+        title: '동원 비엔나',
         price: 92500,
         badge: null,
         quantity: 2,
@@ -28,41 +30,115 @@ export const cart = {
       {
         id: 3,
         image: 'https://picsum.photos/720/960/?image=479',
-        title: 'Coach slim easton black',
-        link: '',
+        title: '에어팟 프로',
         price: 165900,
         badge: null,
         quantity: 3,
         deliveryCharge: 5000,
         ead: '2020-12-30T18:25:43.511Z',
       },
-    ], // 장바구니 리스트
-    totalProductCounts: 0, // 장바구니에 담긴 전체 상품 종류 수
-    selectedProductCounts: 0, // 선택된 상품 종류 수
-    isAllChecked: true, // 전체 선택 체크 여부
+    ],
+    checkAll: false, // 전체 선택
+    checked: [], // 선택된 상품 id
+    totalProductPrice: 0, // (선택된) 전체 상품 금액
+    totalDeliveryPrice: 0, // (선택된) 전체 배송비
   }),
-  getters: {},
+  getters: {
+    // (선택된) 전체 상품 금액
+    totalProductPrice(state) {
+      let sum = 0;
+      for (let i = 0; i < state.checked.length; i++) {
+        for (let j = 0; j < state.items.length; j++) {
+          if (state.items[j].id === state.checked[i]) {
+            sum += state.items[j].price * state.items[j].quantity;
+          }
+        }
+      }
+      state.totalProductPrice = sum;
+      return sum;
+    },
+    // (선택된) 전체 배송비
+    totalDeliveryPrice(state) {
+      let sum = 0;
+      for (let i = 0; i < state.checked.length; i++) {
+        for (let j = 0; j < state.items.length; j++) {
+          if (state.items[j].id === state.checked[i]) {
+            sum += state.items[j].deliveryCharge;
+          }
+        }
+      }
+      state.totalDeliveryPrice = sum;
+      return sum;
+    },
+    // 주문시 총 결제 금액
+    totalPrice(state) {
+      return state.totalProductPrice + state.totalDeliveryPrice;
+    },
+  },
   mutations: {
     DELETE_ITEM(state, id) {
       // items를 업데이트
       // filter : 콜백함수에 지정된 조건에 맞는 요소를 새롭게 반환
       state.items = state.items.filter((item) => item.id !== id);
+      // checkd 업데이트
+      const index = state.checked.findIndex((iid) => iid === id);
+
+      state.checked.splice(index, 1);
+    },
+    // 사용자가 입력한 수량으로 변경
+    UPDATE_QUANTITY(state, { id, quantity }) {
+      const cartItem = state.items.filter((cartItem) => cartItem.id === id);
+
+      // id에 해당하는 상품이 있으면
+      if (cartItem.length !== 0) {
+        // 수량이 0 밑으로 내려가면 해당 아이템을 리스트에서 삭제
+        if (quantity <= 0) {
+          const index = state.checked.findIndex((iid) => iid === id);
+
+          state.checked.splice(index, 1);
+          // 삭제
+          const index2 = state.items.findIndex(
+            (cartItem) => cartItem.id === id
+          );
+
+          state.items.splice(index2, 1);
+        } else {
+          cartItem[0].quantity = quantity;
+        }
+      }
     },
     // 수량 변경 - 상품의 수량을 quantity의 값으로 변경한다.
     CHANGE_QUANTITY(state, { id, quantity }) {
       // id에 해당하는 상품을 참조
+      quantity = Number(quantity);
       const cartItem = state.items.filter((cartItem) => cartItem.id === id);
 
       // id에 해당하는 상품이 있으면
       if (cartItem.length !== 0) {
         // 수량이 0 밑으로 내려가면 해당 아이템을 리스트에서 삭제
         if (cartItem[0].quantity + quantity <= 0) {
-          const index = state.items.findIndex((cartItem) => cartItem.id === id);
+          // checkd 업데이트
+          const index = state.checked.findIndex((iid) => iid === id);
 
-          state.items.splice(index, 1);
+          state.checked.splice(index, 1);
+          // 삭제
+          const index2 = state.items.findIndex(
+            (cartItem) => cartItem.id === id
+          );
+
+          state.items.splice(index2, 1);
         } else {
-          cartItem[0].quantity += quantity;
+          let cc = Number(cartItem[0].quantity);
+          cartItem[0].quantity = cc + quantity;
         }
+      }
+    },
+    UPDATE_CHECKED(state, value) {
+      // 일단 비우고 업데이트
+      state.checked = [];
+
+      for (let i = 0; i < value.length; i++) {
+        state.checked.push(value[i]);
       }
     },
   },
@@ -84,6 +160,10 @@ export const cart = {
         id,
         quantity: -1,
       });
+    },
+    // checked 수정
+    updateChecked({ commit }, value) {
+      commit('UPDATE_CHECKED', value);
     },
   },
 };
