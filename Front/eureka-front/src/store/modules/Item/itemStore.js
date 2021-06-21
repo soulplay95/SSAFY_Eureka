5ea4dbe199a38649e5cc9fcc054dfaf3d9414539
Min-JaeKim <minzminzm@gmail.com>
2021-06-21 21:28:31 +0900

    make carousel, itemlist

--- Front/eureka-front/src/store/modules/Item/itemStore.js
+++ Front/eureka-front/src/store/modules/Item/itemStore.js
@@ -2,6 +2,19 @@ export const itemStore = {
   namespaced: true,
   state: () => ({
     searchText: '',
+    dogImage: [
+      'https://placedog.net/200/200?random',
+      'https://placedog.net/200/200?random',
+      'https://placedog.net/200/200?random',
+      'https://placedog.net/200/200?random',
+      'https://placedog.net/200/200?random',
+      'https://placedog.net/200/200?random',
+      'https://placedog.net/200/200?random',
+      'https://placedog.net/200/200?random',
+      'https://placedog.net/200/200?random',
+      'https://placedog.net/200/200?random',
+    ],
+
   }),
   mutations: {
     SEARCH_ITEM(state, searchText){
@@ -9,6 +22,10 @@ export const itemStore = {
     }
   },
   actions: {
+    
+  // 카테고리 목차를 만든다. 그런데 카테고리는 이중이다
+  // 예를 들어, '잡화 - 신발'이 그 예이다.
+  // 그런데 카테고리는 하나만 주어진다.
     searchItem({ commit }, searchText){
       commit('SEARCH_ITEM', searchText)
     }
