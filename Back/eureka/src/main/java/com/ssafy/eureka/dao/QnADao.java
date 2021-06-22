e2b3bebe3cced208b38dab568a8aaa4daa9d0218
jejin009 <dmsine009@naver.com>
2021-06-23 00:24:16 +0900

    feat: product QnA, 메인QnA, product review 기능 추가
    parameter map으로 할지 / 객체로 할지 정해야함

--- Back/eureka/src/main/java/com/ssafy/eureka/dao/QnADao.java
+++ Back/eureka/src/main/java/com/ssafy/eureka/dao/QnADao.java
@@ -10,13 +10,12 @@ import com.ssafy.eureka.dto.QnA;
 @Mapper
 public interface QnADao {
 
-	public QnA retrieveQnA();
-	public QnA QnADetail();
-	public int writeQnA();
-	public int modifyQnA();
-	public int deleteQnA();
-	
-	public int writeComment();
-	public int updateComment();
-	public int deleteComment();
+	public List<QnA> retrieveQnA();
+	public QnA QnADetail(int qna_id);
+	public int writeQnA(QnA qna);
+	public int modifyQnA(QnA qna);
+	public int deleteQnA(int qna_id);
+	public int writeComment(QnA qna);
+	public int modifyComment(QnA qna);
+	public int deleteComment(int qna_id);
 }
--- Back/eureka/src/main/java/com/ssafy/eureka/service/QnAService.java
+++ Back/eureka/src/main/java/com/ssafy/eureka/service/QnAService.java
@@ -2,17 +2,19 @@ package com.ssafy.eureka.service;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import com.ssafy.eureka.dto.QnA;
 
+@Service
 public interface QnAService {
 
 	public List<QnA> retrieveQnA();
-	public QnA QnADetail();
-	public int writeQnA();
-	public int modifyQnA();
-	public int deleteQnA();
-	
-	public int writeComment();
-	public int updateComment();
-	public int deleteComment();
+	public QnA QnADetail(int no);
+	public int writeQnA(QnA qna);
+	public int modifyQnA(QnA qna);
+	public int deleteQnA(int no);
+	public int writeComment(QnA qna);
+	public int modifyComment(QnA qna);
+	public int deleteComment(int no);
 }
